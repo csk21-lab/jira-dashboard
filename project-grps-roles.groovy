8a43740a9fb3ade0f7c3a336576a0001105273ee
csk21-lab <chandu7324@gmail.com>
2024-11-29 00:40:00 -0600

    Update project-grps-roles.groovy

--- project-grps-roles.groovy
+++ project-grps-roles.groovy
@@ -13,53 +13,50 @@ String finalResult = ""
 def userList = []
 def rolesMissingGroup = []  // List to store roles with missing groups
 
-// Specify the project keys you want to check
-def projectkeys = ["ABC", "DEF"]
-projectkeys.each { projectKey ->
-    // Get the project using the Project Manager
-    def project = ComponentAccessor.getProjectManager().getProjectByKey(projectKey)
-    ProjectRoleManager projectRoleManager = ComponentAccessor.getComponent(ProjectRoleManager)
-    GroupManager groupManager = ComponentAccessor.getGroupManager()
-    UserManager userManager = ComponentAccessor.getUserManager() as UserManager
-    def groupRoles = projectRoleManager.getProjectRoles(project)
+// Specify the project key you want to check (for one project)
+def projectKey = "ABC"  // Replace with your project key
+def project = ComponentAccessor.getProjectManager().getProjectByKey(projectKey)
 
-    result += "Project Name: " + project.name + "\r"
-    result += "Project Key: " + project.key + "\r\n"
+ProjectRoleManager projectRoleManager = ComponentAccessor.getComponent(ProjectRoleManager)
+GroupManager groupManager = ComponentAccessor.getGroupManager()
+UserManager userManager = ComponentAccessor.getUserManager() as UserManager
+def groupRoles = projectRoleManager.getProjectRoles(project)
 
-    groupRoles.each { role ->
-        result += "<b>" + role.toString() + "</b>\r"
-        def projectRoleMembers = projectRoleManager.getProjectRoleActors(role, project)
+result += "Project Name: " + project.name + "\r"
+result += "Project Key: " + project.key + "\r\n"
 
-        // Check if a group is associated with the role using HAPI method
-        def groupRoleActors = projectRoleManager.getProjectRoleActorsByRoleType(role, project, ProjectRoleActor.GroupRoleActor)
-        if (groupRoleActors.isEmpty()) {
-            // If no group is associated with this role, add it to the missing group list
-            rolesMissingGroup << role.toString()
-        } else {
-            groupRoleActors.each { groupRoleActor ->
-                result += "Group: " + groupRoleActor.getGroup().getName() + "\r"
-            }
-        }
+groupRoles.each { role ->
+    result += "<b>" + role.toString() + "</b>\r"
+    def projectRoleMembers = projectRoleManager.getProjectRoleActors(role, project)
 
-        // Skipping the part for displaying user information if not needed
-        projectRoleMembers.each { projectRoleActor ->
-            def user = projectRoleActor.getUser()
-            result += "User: " + user.getUsername() + "\r"
-
-            // Using HAPI to check if the user is a member of a specific role in the project
-            def isMember = user.isMemberOfRole(role, project)
-            if (isMember) {
-                result += "User is a member of the ${role} role in project ${project.name}\r"
-            } else {
-                result += "User is NOT a member of the ${role} role in project ${project.name}\r"
-            }
+    // Check if a group is associated with the role using HAPI method
+    def groupRoleActors = projectRoleManager.getProjectRoleActorsByRoleType(role, project, ProjectRoleActor.GroupRoleActor)
+    if (groupRoleActors.isEmpty()) {
+        // If no group is associated with this role, add it to the missing group list
+        rolesMissingGroup << role.toString()
+    } else {
+        groupRoleActors.each { groupRoleActor ->
+            result += "Group: " + groupRoleActor.getGroup().getName() + "\r"
         }
     }
 
-    finalResult += result
-    finalResult += "-------------------------\r\n"
-    result = ""
+    // Skipping the part for displaying user information if not needed
+    projectRoleMembers.each { projectRoleActor ->
+        def user = projectRoleActor.getUser()
+        result += "User: " + user.getUsername() + "\r"
+
+        // Using HAPI to check if the user is a member of a specific role in the project
+        def isMember = user.isMemberOfRole(role, project)
+        if (isMember) {
+            result += "User is a member of the ${role} role in project ${project.name}\r"
+        } else {
+            result += "User is NOT a member of the ${role} role in project ${project.name}\r"
+        }
+    }
 }
+
+finalResult += result
+finalResult += "-------------------------\r\n"
 
 // If any roles are missing corresponding groups, display the missing roles
 if (rolesMissingGroup.size() > 0) {
