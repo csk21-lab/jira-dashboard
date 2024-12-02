e9149186a9987c32bf3501d80f44aadc5f7cccde
csk21-lab <chandu7324@gmail.com>
2024-12-01 21:10:23 -0600

    Update project-roles-groups.groovy

--- project-roles-groups.groovy
+++ project-roles-groups.groovy
@@ -12,15 +12,16 @@ projects.each { project ->
     def projectRoles = projectRoleManager.getProjectRoles(project)
     def existingRoleNames = projectRoles.collect { it.getName() }
     def missingRoles = requiredRoles.findAll { !existingRoleNames.contains(it) }
-    String missingRolesInfo = missingRoles.isEmpty() ? "" : " Missing Roles: ${missingRoles.join(', ')}"
-
-    if (projectRoles.isEmpty()) {
-        log.warn("${project.getName()} - No roles defined${missingRolesInfo}")
-    } else {
+    
+    if (!missingRoles.isEmpty()) {
+        String missingRolesInfo = "Missing Roles: ${missingRoles.join(', ')}"
+        log.warn("${project.getName()} - ${missingRolesInfo}")
+        
         projectRoles.each { role ->
             def groups = projectRoleManager.getProjectRoleActors(role, project).getRoleActorsByType(ProjectRoleActor.GROUP_ROLE_ACTOR_TYPE)
             String groupInfo = groups.isEmpty() ? "No groups" : "Groups exist"
-            log.warn("${project.getName()} - ${role.getName()} : ${groupInfo}${missingRolesInfo}")
+            log.warn("${project.getName()} - ${role.getName()} : ${groupInfo}")
         }
     }
 }
+
