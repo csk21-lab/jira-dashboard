07951bf276d6125de0b1ec316619ee891711f67d
csk21-lab <chandu7324@gmail.com>
2024-12-01 19:30:26 -0600

    Update project-roles-groups.groovy

--- project-roles-groups.groovy
+++ project-roles-groups.groovy
@@ -10,17 +10,17 @@ def requiredRoles = ["Administrators", "Developers", "Project Manager"]
 
 projects.each { project ->
     def projectRoles = projectRoleManager.getProjectRoles(project)
-    def roleNames = projectRoles.collect { it.getName() }
-    def missingRoles = requiredRoles.findAll { !roleNames.contains(it) }
+    def existingRoleNames = projectRoles.collect { it.getName() }
+    def missingRoles = requiredRoles.findAll { !existingRoleNames.contains(it) }
     String missingRolesInfo = missingRoles.isEmpty() ? "" : " Missing Roles: ${missingRoles.join(', ')}"
-
-    projectRoles.each { role ->
-        def groups = projectRoleManager.getProjectRoleActors(role, project).getRoleActorsByType(ProjectRoleActor.GROUP_ROLE_ACTOR_TYPE)
-        String groupInfo = groups.isEmpty() ? "No groups" : "Groups exist"
-        log.warn("${project.getName()} - ${role.getName()} : ${groupInfo}${missingRolesInfo}")
-    }
 
     if (projectRoles.isEmpty()) {
         log.warn("${project.getName()} - No roles defined${missingRolesInfo}")
+    } else {
+        projectRoles.each { role ->
+            def groups = projectRoleManager.getProjectRoleActors(role, project).getRoleActorsByType(ProjectRoleActor.GROUP_ROLE_ACTOR_TYPE)
+            String groupInfo = groups.isEmpty() ? "No groups" : "Groups exist"
+            log.warn("${project.getName()} - ${role.getName()} : ${groupInfo}${missingRolesInfo}")
+        }
     }
 }
