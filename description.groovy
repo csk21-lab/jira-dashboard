70811decf908629565befecb28aee86e42cb7c15
csk21-lab <chandu7324@gmail.com>
2024-12-24 11:01:50 -0600

    Create description.groovy

---
+++ description.groovy
@@ -0,0 +1,35 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.Issue
+import com.atlassian.jira.issue.customfields.manager.CustomFieldManager
+import com.atlassian.jira.issue.fields.CustomField
+
+// Get the current issue
+Issue issue = ComponentAccessor.getIssueManager().getIssueObject("ISSUE-KEY") // Replace with your issue key
+
+// Get the custom field manager
+CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager()
+
+// Define your custom fields
+CustomField customField1 = customFieldManager.getCustomFieldObjectByName("Custom Field 1")
+CustomField customField2 = customFieldManager.getCustomFieldObjectByName("Custom Field 2")
+CustomField customField3 = customFieldManager.getCustomFieldObjectByName("Custom Field 3")
+
+// Get the values of the custom fields
+def customFieldValue1 = issue.getCustomFieldValue(customField1)
+def customFieldValue2 = issue.getCustomFieldValue(customField2)
+def customFieldValue3 = issue.getCustomFieldValue(customField3)
+
+// Create the table in Markdown format
+def table = """
+| Field Name   | Description        |
+|--------------|--------------------|
+| Custom Field 1 | ${customFieldValue1 ?: 'N/A'} |
+| Custom Field 2 | ${customFieldValue2 ?: 'N/A'} |
+| Custom Field 3 | ${customFieldValue3 ?: 'N/A'} |
+"""
+
+// Update the issue description
+issue.setDescription(issue.getDescription() + "\n\n" + table)
+
+// Save the changes
+ComponentAccessor.getIssueManager().updateIssue(ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser(), issue, com.atlassian.jira.event.type.EventDispatchOption.DO_NOT_DISPATCH, false)
