3a52606ab2f37e9b57f9788915a0c9ebe4fedf48
csk21-lab <chandu7324@gmail.com>
2025-02-05 14:16:37 -0600

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -3,7 +3,8 @@
 # Remote server details
 REMOTE_USER="your_username"
 REMOTE_HOST="remote_server_ip_or_hostname"
+REMOTE_PASSWORD="your_password"  # WARNING: Storing passwords in plaintext is not secure.
 REMOTE_COMMAND="curl http://example.com/api/endpoint"
 
-# Execute the curl command remotely using SSH
-ssh ${REMOTE_USER}@${REMOTE_HOST} "${REMOTE_COMMAND}"
+# Execute the curl command remotely using SSH with sshpass
+sshpass -p "${REMOTE_PASSWORD}" ssh ${REMOTE_USER}@${REMOTE_HOST} "${REMOTE_COMMAND}"
