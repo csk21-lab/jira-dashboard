13059e88880228faa06fb4b68de60f4ff0b33c1a
csk21-lab <chandu7324@gmail.com>
2025-02-24 15:55:37 -0600

    Update updated-desc.groovy

--- updated-desc.groovy
+++ updated-desc.groovy
@@ -2,19 +2,15 @@ import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.bc.issue.search.SearchService
 import com.atlassian.jira.issue.IssueManager
 import com.atlassian.jira.issue.MutableIssue
-import com.atlassian.jira.issue.search.SearchResults
 import com.atlassian.jira.user.ApplicationUser
 import com.atlassian.jira.web.bean.PagerFilter
-import com.atlassian.jira.event.type.EventDispatchOption
+import com.onresolve.scriptrunner.runner.util.UserMessageUtil
 
 def issueManager = ComponentAccessor.getIssueManager()
-def customFieldManager = ComponentAccessor.getCustomFieldManager()
 def searchService = ComponentAccessor.getComponent(SearchService)
 def loggedInUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser as ApplicationUser
 
-def jqlQuery = """
-    issuekey in (ABC-6592, ABC-1540)
-"""
+def jqlQuery = "issuekey in (ABC-6592, ABC-1540)"
 
 try {
     def parseResult = searchService.parseQuery(loggedInUser, jqlQuery)
@@ -33,11 +29,13 @@ try {
             if (issueDescription.contains("ABC-338")) {
                 issueDescription = issueDescription.replace("ABC-338", "ABC-343")
                 log.warn("IF Block : ${issueDescription}")
-            }
 
-            def mutableIssue = issueManager.getIssueObject(issue.id) as MutableIssue
-            mutableIssue.setDescription(issueDescription)
-            issueManager.updateIssue(loggedInUser, mutableIssue, EventDispatchOption.DO_NOT_DISPATCH, false)
+                // Update the issue description using ScriptRunner's updateIssue method
+                def mutableIssue = issueManager.getIssueObject(issue.id) as MutableIssue
+                mutableIssue.setDescription(issueDescription)
+                ComponentAccessor.getIssueManager().updateIssue(loggedInUser, mutableIssue, EventDispatchOption.ISSUE_UPDATED, false)
+                UserMessageUtil.success("Successfully updated the issue with key ${issue.key}")
+            }
         }
     } else {
         log.warn("No issues found for the JQL query: ${jqlQuery}")
