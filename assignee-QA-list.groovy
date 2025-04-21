e80ee39f76523945c8b850f65e11477597470115
csk21-lab <chandu7324@gmail.com>
2025-04-20 21:42:29 -0500

    Update assignee-QA-list.groovy

--- assignee-QA-list.groovy
+++ assignee-QA-list.groovy
@@ -2,16 +2,22 @@
 def issue = getIssueContext()
 
 // Get the assignee of the issue
-def assignee = issue.assignee
+def assignee = issue?.assignee
 
 // Get the custom field for the QA Analyst user picker
 def qaAnalystField = getFieldByName("QA Analyst") // Replace with the exact name of the field
 
-// Get the current list of users in the QA Analyst field
-def availableUsers = qaAnalystField.getFieldValue()
+if (qaAnalystField) {
+    // Get the current list of users in the QA Analyst field
+    def availableUsers = qaAnalystField.getFieldValue()
 
-// If assignee is in the QA Analyst field, remove them from the options
-if (assignee) {
-    def updatedUsers = availableUsers.findAll { it != assignee }
-    qaAnalystField.setFieldValue(updatedUsers)
+    if (availableUsers && assignee) {
+        // Remove the assignee from the options
+        def updatedUsers = availableUsers.findAll { it != assignee }
+        qaAnalystField.setFieldValue(updatedUsers)
+    } else {
+        log.debug("Assignee or available users are null. Assignee: ${assignee}, Available Users: ${availableUsers}")
+    }
+} else {
+    log.debug("QA Analyst field not found.")
 }
