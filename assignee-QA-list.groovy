064c5666222b91aa8720213cf3259b7e1ecc9585
csk21-lab <chandu7324@gmail.com>
2025-04-21 20:11:20 -0500

    Update assignee-QA-list.groovy

--- assignee-QA-list.groovy
+++ assignee-QA-list.groovy
@@ -1,30 +1,33 @@
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.issue.MutableIssue
 import com.atlassian.jira.user.ApplicationUser
+import com.atlassian.jira.issue.MutableIssue
 
-// Get the issue object
-def issue = getIssueContext()
+// Get the issue object (for the context of this behavior)
+def issue = underlyingIssue
 
-// Get the assignee of the issue
+// Get the current assignee of the issue
 def assignee = issue?.assignee
 
-// Get the custom field for the QA Analyst user picker
-def qaAnalystField = getFieldByName("QA Analyst") // Replace with the exact name of the field
+// Get the "QA Analyst" field
+def qaAnalystField = getFieldByName("QA Analyst")  // Replace with the exact name of the field if needed
 
+// Check if the "QA Analyst" field and assignee are not null
 if (qaAnalystField && assignee) {
-    // Get the user manager
-    def userManager = ComponentAccessor.getUserManager()
-    
-    // Get all users in the user picker options for "QA Analyst"
-    def userPickerOptions = qaAnalystField.getField().getCustomFieldType().getConfigurationSchemes()
+    // Get the current user being selected in the "QA Analyst" field
+    def currentValue = qaAnalystField.getValue()
 
-    // Remove the assignee from the options list
-    def updatedOptions = userPickerOptions.findAll { user ->
-        return user.username != assignee.username // Remove the assignee from the list
+    // Check if the current value of the QA Analyst field is the same as the assignee
+    if (currentValue?.username == assignee.username) {
+        // If so, remove the assignee from the "QA Analyst" field options
+        qaAnalystField.setFieldValue(null) // Clear the value if the assignee is selected
     }
-    
-    // Set the updated options
-    qaAnalystField.setFieldValue(updatedOptions)
-} else {
-    log.debug("Assignee or QA Analyst field is null. Assignee: ${assignee}, QA Analyst Field: ${qaAnalystField}")
+
+    // Ensure the assignee is not available in the user picker options
+    def users = qaAnalystField.getFieldOptions()
+    def filteredUsers = users.findAll { user ->
+        user.username != assignee.username  // Remove the assignee from the options
+    }
+
+    // Update the user picker options
+    qaAnalystField.setFieldOptions(filteredUsers)
 }
