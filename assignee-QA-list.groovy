152bf2e17155bf2d78189b0b790be802cd791328
csk21-lab <chandu7324@gmail.com>
2025-04-21 22:54:12 -0500

    Update assignee-QA-list.groovy

--- assignee-QA-list.groovy
+++ assignee-QA-list.groovy
@@ -21,23 +21,27 @@ if (qaAnalystField && assignee) {
 
     // Check if the current value of the QA Analyst field is the same as the assignee's username
     if (currentValue == assignee.key) {
-        log.debug("Assignee is the same as selected in 'QA Analyst'. Clearing the 'QA Analyst' field value.")
-        // If so, remove the assignee from the "QA Analyst" field options
-        qaAnalystField.setFieldValue(null) // Clear the value if the assignee is selected
-    }
+        log.debug("Assignee is the same as selected in 'QA Analyst'. Showing error on screen.")
+        
+        // Show an error message on the screen if the assignee is selected in 'QA Analyst'
+        qaAnalystField.setErrorMessage("The assignee cannot be selected as the QA Analyst.")
+        
+        // Optionally, clear the value of the "QA Analyst" field
+        qaAnalystField.setFieldValue(null)  // Clear the value if the assignee is selected
+    } else {
+        // If the assignee is not selected, remove the assignee from the options
+        def users = qaAnalystField.getFieldOptions()
+        log.debug("Total available users in QA Analyst: ${users.size()}")
+        
+        def filteredUsers = users.findAll { user ->
+            user.key != assignee.key  // Remove the assignee from the options by comparing the user key
+        }
+        log.debug("Filtered users (Assignee removed): ${filteredUsers.size()} remaining options.")
 
-    // Ensure the assignee is not available in the user picker options
-    def users = qaAnalystField.getFieldOptions()
-    log.debug("Total available users in QA Analyst: ${users.size()}")
-    
-    def filteredUsers = users.findAll { user ->
-        user.key != assignee.key  // Remove the assignee from the options by comparing the user key
+        // Update the user picker options
+        qaAnalystField.setFieldOptions(filteredUsers)
+        log.debug("QA Analyst field options updated.")
     }
-    log.debug("Filtered users (Assignee removed): ${filteredUsers.size()} remaining options.")
-
-    // Update the user picker options
-    qaAnalystField.setFieldOptions(filteredUsers)
-    log.debug("QA Analyst field options updated.")
 } else {
     log.warn("QA Analyst field or assignee is null. Assignee: ${assignee?.username}, QA Analyst Field: ${qaAnalystField?.getFieldId()}")
 }
