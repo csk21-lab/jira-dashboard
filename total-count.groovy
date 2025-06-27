8557ee2ef4d5cf3f8a4945536254c923e98bc9bd
csk21-lab <chandu7324@gmail.com>
2025-06-27 07:27:32 -0500

    Update total-count.groovy

--- total-count.groovy
+++ total-count.groovy
@@ -1,25 +1,31 @@
 import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.search.SearchProvider
+import com.atlassian.jira.web.bean.PagerFilter
+import com.atlassian.query.Query
+import com.atlassian.jira.jql.parser.JqlQueryParser
 import com.riadalabs.jira.plugins.insight.services.model.ObjectBean
 
-def issueKey = "ABC-124"
-def customFieldId = "customfield_00100"  // Replace with your actual Insight field ID
+def jql = 'project = ABC AND issuetype = Bug'  // Replace with your JQL
+def customFieldId = "customfield_00100"        // Replace with your Insight field ID
 
-def issueManager = ComponentAccessor.issueManager
+def searchService = ComponentAccessor.getComponent(SearchProvider)
+def user = ComponentAccessor.jiraAuthenticationContext.loggedInUser
+def jqlParser = ComponentAccessor.getComponent(JqlQueryParser)
 def customFieldManager = ComponentAccessor.customFieldManager
 
-def issue = issueManager.getIssueObject(issueKey)
 def assetField = customFieldManager.getCustomFieldObject(customFieldId)
+Query query = jqlParser.parseQuery(jql)
+def results = searchService.search(query, user, PagerFilter.getUnlimitedFilter())
 
-def selectedAssets = issue.getCustomFieldValue(assetField)
+results.issues.each { issue ->
+    def assetValue = issue.getCustomFieldValue(assetField)
+    int count = 0
 
-int totalCount = 0
+    if (assetValue instanceof List) {
+        count = assetValue.size()
+    } else if (assetValue instanceof ObjectBean) {
+        count = 1
+    }
 
-if (selectedAssets instanceof List) {
-    totalCount = selectedAssets.size()
-} else if (selectedAssets instanceof ObjectBean) {
-    totalCount = 1  // Single selected asset
-} else {
-    totalCount = 0  // Field is empty
+    log.warn("Issue ${issue.key} has ${count} asset object(s) in field '${assetField.name}'")
 }
-
-log.warn("Total number of asset objects in field '${assetField.name}': ${totalCount}")
