e8a9b926cb3aede5051c0df73f6aee70994ff8ca
csk21-lab <chandu7324@gmail.com>
2025-07-21 22:58:21 -0500

    Create neverLoggedInUsers.groovy

---
+++ neverLoggedInUsers.groovy
@@ -0,0 +1,54 @@
+import com.atlassian.jira.security.login.LoginManager
+import com.atlassian.crowd.manager.directory.DirectoryManager
+import com.atlassian.jira.bc.user.UserService
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.user.ApplicationUser
+import com.atlassian.jira.issue.search.SearchProvider
+import com.atlassian.jira.jql.parser.JqlQueryParser
+import com.atlassian.jira.web.bean.PagerFilter
+
+def userManager = ComponentAccessor.userManager
+def directoryManager = ComponentAccessor.getComponent(DirectoryManager)
+def loginManager = ComponentAccessor.getComponent(LoginManager)
+def groupManager = ComponentAccessor.getGroupManager()
+def userService = ComponentAccessor.getComponent(UserService)
+def loggedInUser = ComponentAccessor.jiraAuthenticationContext.loggedInUser
+def runLog = []
+
+def internalDirectory = 10001
+def daysOfInactivity = 350
+def neverLoggedInUsers = []
+def today = new Date()
+def cutoffDate = today - daysOfInactivity
+
+// Iterate over all users
+userManager.allApplicationUsers.each { user ->
+    def directory = directoryManager.findDirectoryById(user.directoryId)
+    if (directory.id == internalDirectory) {
+        // Check if the user is part of the 'jira-software-users' group
+        def groups = groupManager.getGroupsForUser(user)
+        if (groups.any { it.name == 'jira-software-users' }) {
+            // Get the login information for the user
+            def loginInfo = loginManager.getLoginInfo(user.username)
+            if (loginInfo) {
+                if (loginInfo.lastLoginTime == null) {
+                    // User has never logged in
+                    neverLoggedInUsers << user.username
+                } else {
+                    def lastLogon = new Date(loginInfo.lastLoginTime)
+                    if (lastLogon.before(cutoffDate)) {
+                        // User is inactive but has logged in before
+                        neverLoggedInUsers << user.username
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Display the results
+def neverLoggedInList = neverLoggedInUsers.join(", ")
+def totalNeverLoggedInUsers = neverLoggedInUsers.size()
+
+log.warn("Total number of users who have never logged in and are in 'jira-software-users' group: ${totalNeverLoggedInUsers}")
+log.warn("List of users who have never logged in: \n${neverLoggedInList}")
