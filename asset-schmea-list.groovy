077f2dc7f7d75fe8d47c991b045435af2c1d908d
csk21-lab <chandu7324@gmail.com>
2025-09-25 10:31:22 -0500

    Add null and empty checks for schemas

--- asset-schmea-list.groovy
+++ asset-schmea-list.groovy
@@ -2,8 +2,12 @@ def objectSchemaFacade = ComponentAccessor.getOSGiComponentInstanceOfType(
     com.riadalabs.jira.plugins.insight.channel.external.api.facade.ObjectSchemaFacade
 )
 def schemas = objectSchemaFacade.findAll()
-println "schemas class: " + schemas.getClass().name
-if (schemas != null && schemas.size() > 0) {
+if (schemas == null) {
+    println "schemas is null"
+} else if (schemas.size() == 0) {
+    println "schemas is empty"
+} else {
+    println "schemas count: " + schemas.size()
     println "First schema object class: " + schemas[0].getClass().name
     println "First schema properties: " + schemas[0].properties
 }
