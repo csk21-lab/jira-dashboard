334e9cff1673d17bc31804124dc8563904b335b4
csk21-lab <chandu7324@gmail.com>
2025-11-15 22:34:14 -0600

    Refactor multi-users.groovy for user management

--- multi-users.groovy
+++ multi-users.groovy
@@ -1,30 +1,41 @@
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.issue.ModifiedValue
 import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
+import com.atlassian.jira.bc.issue.search.SearchService
+import com.atlassian.jira.web.bean.PagerFilter
 
+// Parameters
+def userKeyToAppend = "userkey"  // User key of the user you want to add
+def multiUserFieldName = "Multi User Picker Field Name"  // Update with your custom field name
+def jqlQuery = "project = ABC AND status = Open"  // Update with your JQL
+
+// Components
 def customFieldManager = ComponentAccessor.getCustomFieldManager()
-def issueManager = ComponentAccessor.getIssueManager()
 def userManager = ComponentAccessor.getUserManager()
-def changeHolder = new DefaultIssueChangeHolder()
-def searchService = ComponentAccessor.getComponent(com.atlassian.jira.bc.issue.search.SearchService)
+def searchService = ComponentAccessor.getComponent(SearchService)
 def authenticationContext = ComponentAccessor.jiraAuthenticationContext
-def user = authenticationContext.loggedInUser
+def runningUser = authenticationContext.loggedInUser
+def changeHolder = new DefaultIssueChangeHolder()
 
-// JQL query to filter issues
-def jqlQuery = "project = ABC AND status = Open" // replace with your JQL
-def parseResult = searchService.parseQuery(user, jqlQuery)
-if (!parseResult.isValid()) {
-    return "Invalid JQL query"
-}
+// Validate JQL
+def parseResult = searchService.parseQuery(runningUser, jqlQuery)
+assert parseResult.isValid() : "Invalid JQL query: ${parseResult.errors}"
 
+// Search issues
 def query = parseResult.getQuery()
-def searchResult = searchService.search(user, query, com.atlassian.jira.web.bean.PagerFilter.getUnlimitedFilter())
+def searchResult = searchService.search(runningUser, query, PagerFilter.getUnlimitedFilter())
+def issues = searchResult.getIssues()
 
-def multiUserField = customFieldManager.getCustomFieldObjectByName("Multi User Picker Field Name")
-def newUser = userManager.getUserByKey("usernameToAdd")  // User to append
+def multiUserField = customFieldManager.getCustomFieldObjectByName(multiUserFieldName)
+assert multiUserField : "Custom field not found: ${multiUserFieldName}"
 
-searchResult.getIssues().each { issue ->
+def userToAppend = userManager.getUserByKey(userKeyToAppend)
+assert userToAppend : "User not found: ${userKeyToAppend}"
+
+issues.each { issue ->
     def existingUsers = issue.getCustomFieldValue(multiUserField) ?: []
-    def updatedUsers = existingUsers.toSet() + [newUser]
-    multiUserField.updateValue(null, issue, new ModifiedValue(existingUsers, updatedUsers.toList()), changeHolder)
+    def updatedUsers = (existingUsers.toSet() + userToAppend).toList()
+    multiUserField.updateValue(null, issue, new ModifiedValue(existingUsers, updatedUsers), changeHolder)
 }
+
+return "Appended user ${userKeyToAppend} to ${issues.size()} issues matching JQL: ${jqlQuery}"
