912eaad4905516e6bd9f01db064636710541fded
csk21-lab <chandu7324@gmail.com>
2025-11-15 23:05:38 -0600

    Refactor asset field setting with object type name
    Updated IQL query to use object type name instead of ID and improved logging.

--- set-asset-field.groovy
+++ set-asset-field.groovy
@@ -1,6 +1,6 @@
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.issue.MutableIssue
 import com.riadalabs.jira.plugins.insight.channel.external.api.facade.IQLFacade
+import com.atlassian.jira.issue.MutableIssue
 import com.atlassian.jira.issue.fields.CustomField
 import org.apache.log4j.Logger
 
@@ -9,7 +9,7 @@ def log = Logger.getLogger("com.acme.SetAssetFieldSpecificIssue")
 String targetIssueKey = "ABC-212"
 String assetsCustomFieldName = "ON"  
 String assetObjectKey = "ABC-912536"
-Long objectTypeId = 103
+String objectTypeName = "YourObjectTypeName" // <-- Replace with your actual type name
 
 def issueManager = ComponentAccessor.getIssueManager()
 def customFieldManager = ComponentAccessor.getCustomFieldManager()
@@ -17,12 +17,14 @@ def iqlFacade = ComponentAccessor.getOSGiComponentInstanceOfType(IQLFacade)
 
 def issue = issueManager.getIssueByCurrentKey(targetIssueKey)
 def assetsCustomField = customFieldManager.getCustomFieldObjectByName(assetsCustomFieldName)
-def objectBeans = iqlFacade.findObjectsByIQL(objectTypeId, "Key = \"$assetObjectKey\"")
+def iql = 'objectType = "' + objectTypeName + '" AND Key = "' + assetObjectKey + '"'
+
+def objectBeans = iqlFacade.findObjectsByIQL(iql)
 def assetObjectBean = objectBeans ? objectBeans[0] : null
 
 if (issue && assetsCustomField && assetObjectBean) {
-    issue.setCustomFieldValue(assetsCustomField, assetObjectBean) // For multi-value, use [assetObjectBean]
+    issue.setCustomFieldValue(assetsCustomField, assetObjectBean) // [assetObjectBean] for multi-value fields
     log.warn("Set asset '${assetObjectBean.name}' on issue '${issue.key}'")
 } else {
-    log.warn("FAILED: issue=${issue!=null}, field=${assetsCustomField!=null}, bean=${assetObjectBean!=null}")
+    log.warn("FAILED: issue=${issue != null}, field=${assetsCustomField != null}, bean=${assetObjectBean != null}, IQL='$iql'")
 }
