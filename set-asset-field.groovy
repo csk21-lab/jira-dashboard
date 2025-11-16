6708dcf80f5ec2852c3278ec98d15ce710b63303
csk21-lab <chandu7324@gmail.com>
2025-11-15 21:59:45 -0600

    Implement asset field setting for specific issue
    Added logic to set asset value on a specific issue using custom fields.

--- set-asset-field.groovy
+++ set-asset-field.groovy
@@ -1,4 +1,44 @@
 import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.issue.IssueManager
+import com.atlassian.jira.issue.fields.CustomField
+import com.riadalabs.jira.plugins.insight.channel.external.api.facade.ObjectFacade
+import org.apache.log4j.Logger
+
+def log = Logger.getLogger("com.acme.SetAssetFieldSpecificIssue")
+
+// --- Configuration ---
+String targetIssueKey = "ABC-212"
+String assetsCustomFieldName = "ON"           // Your Assets custom field name
+String assetObjectKey = "XYZ-912536"          // Your Insight Object Key
+Long objectTypeId = 101                       // <-- REPLACE with your Insight Object Type ID
+
+def issueManager = ComponentAccessor.getIssueManager()
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+def objectFacade = ComponentAccessor.getOSGiComponentInstanceOfType(ObjectFacade)
+
+// Fetch the MutableIssue object from the issue key
+def issue = issueManager.getIssueByCurrentKey(targetIssueKey)
+def assetsCustomField = customFieldManager.getCustomFieldObjectByName(assetsCustomFieldName)
+
+// Load the ObjectBean by key
+def assetObjectBean = objectFacade.loadObjectBeanByKey(objectTypeId, assetObjectKey)
+
+// Set the custom field value if everything is found
+if (issue && assetsCustomField && assetObjectBean) {
+    issue.setCustomFieldValue(assetsCustomField, assetObjectBean) // For single-value field
+    // For multi-value field:
+    // issue.setCustomFieldValue(assetsCustomField, [assetObjectBean])
+    log.warn("Set asset '${assetObjectBean.name}' on issue '${issue.key}'")
+} else {
+    log.warn("FAILED: issue=${issue!=null}, field=${assetsCustomField!=null}, bean=${assetObjectBean!=null}")
+}
+
+
+
+
+/*
+import com.atlassian.jira.component.ComponentAccessor
 import com.riadalabs.jira.plugins.insight.channel.external.api.facade.ObjectFacade
 
 def customFieldManager = ComponentAccessor.getCustomFieldManager()
@@ -15,3 +55,5 @@ def assetObjectBean = objectFacade.loadObjectBean(assetObjectKey)
 if (assetsCustomField && assetObjectBean) {
     issue.setCustomFieldValue(assetsCustomField, [assetObjectBean])
 }
+
+*/
